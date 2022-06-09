99152503bee6ec917731bb7d445d3d9154726602
rtavares <ricardotavaresit@gmail.com>
2022-06-09 21:01:07 +0100

    create generateQuestion

--- src/components/Animations/Question.vue
+++ src/components/Animations/Question.vue
@@ -1,5 +1,11 @@
 <template>
-  <h2 v-text="title"></h2>
+  <div>
+    <h2 v-text="question"></h2>
+    <form v-on:submit.prevent="checkResult">
+      <input type="text" placeholder="Awswer" v-model="reply" />
+      <button type="submit">Awswer</button>
+    </form>
+  </div>
 </template>
 
 
@@ -8,8 +14,33 @@ export default {
   data() {
     return {
       title: "Question Component",
-      question:"Generate question...."
+      question: "Generate question....",
+      result: 0,
+      reply: "",
     };
+  },
+  created() {
+    this.generateQuestion();
+  },
+  methods: {
+    generateQuestion() {
+      let numberOne = this.generateRandonNumber(1, 100);
+      let numberTwo = this.generateRandonNumber(1, 100);
+
+      this.result = numberOne + numberTwo;
+
+      this.question = `What is th sum of ${numberOne} and ${numberTwo}`;
+    },
+    generateRandonNumber(min, max) {
+      return Math.round(Math.random() * max) + min;
+    },
+    checkResult() {
+      if (this.reply == this.result) {
+        alert("Correct!");
+      } else {
+        alert("Wrong");
+      }
+    },
   },
 };
 </script>
