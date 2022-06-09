c8a6544e7849fcc0f32af723b8b18b97af1a62c1
rtavares <ricardotavaresit@gmail.com>
2022-06-09 19:31:46 +0100

    create Quiz component

---
+++ src/components/Animations/Quiz.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 v-text="title"></h2>
+    <app-question />
+  </div>
+</template>
+
+<script>
+import Question from "./Question.vue";
+export default {
+  data() {
+    return {
+      title: "Component Quiz",
+    };
+  },
+  components: {
+    "app-question": Question,
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
