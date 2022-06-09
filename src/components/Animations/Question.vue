b3c45115a4cdc6828946c50e023a6861cf03d25e
rtavares <ricardotavaresit@gmail.com>
2022-06-09 19:31:26 +0100

    create Question component

---
+++ src/components/Animations/Question.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 v-text="title"></h2>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Question Component",
+      question:"Generate question...."
+    };
+  },
+};
+</script>
